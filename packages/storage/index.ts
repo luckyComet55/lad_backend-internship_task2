03fd8a76f964a7782ec10e149d5da153742cc5b2
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2022-09-14 17:33:30 +0300

    Added some data validation before database query

--- packages/storage/index.ts
+++ packages/storage/index.ts
@@ -4,7 +4,17 @@ import { connect2Nats, closeNats } from "../common/index.js";
 import { HapiServerConnectionError } from "../common/index.js";
 import { stopRoute } from "./handlers/dev-stop.js";
 import { DataBase } from "./repository/dbconn.js";
-import {Fact} from "./repository/entity/Fact.js";
+import { Fact } from "./repository/entity/Fact.js";
+
+function validateData(data: string): boolean {
+    const dataSchema = /^f_(?<colName>id|cont):(?<colData>[^]*)$/;
+    const parsedData = data.match(dataSchema);
+    if(parsedData === null || parsedData.groups === undefined) return false;
+    if(parsedData.groups.colName === "id") {
+        return /\d+/.test(parsedData.groups.colData);
+    }
+    return true;
+}
 
 async function createServer(): Promise<Server> {
     const server: Server = new Server({
@@ -49,8 +59,10 @@ async function runServer() {
         const sc = StringCodec();
         const sub = nc.subscribe("storage.api.request.find");
         for await (const m of sub) {
-            console.log(`${m.subject.split(".")[3]}: ${sc.decode(m.data).split(":")}`);
-            const [key, val] = sc.decode(m.data).split(":");
+            console.log(`[${sub.getProcessed()}]${m.subject.split(".")[3]}: ${sc.decode(m.data).split(":")}`);
+            const data = sc.decode(m.data);
+            if(!validateData(data)) continue;
+            const [key, val] = data.split(":");
             const dbQueryRes: Fact | null = await dataBase.find(key, val);
             if(dbQueryRes === null) {
                 console.log("Query returned nothing");
